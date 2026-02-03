6d0bfa18fa1fc6c99931bb9c41420ac7d39e29ed
Michael Boateng Mensah <mkboatmens17@gmail.com>
2026-02-03 09:32:35 +0000

    feat: implement scoreboard logic

---
+++ html-css-js/basketball-scoreboard/ball.js
@@ -0,0 +1,44 @@
+let currentHomeScore = 0;
+let currentGuestScore = 0;
+
+// manipulate home scores
+const displayHomeScore = document.getElementById("home-score");
+displayHomeScore.textContent = currentHomeScore;
+
+function add1Point() {
+  currentHomeScore = currentHomeScore + 1;
+  displayHomeScore.textContent = currentHomeScore;
+}
+
+function add2Point() {
+  currentHomeScore = currentHomeScore + 2;
+  displayHomeScore.textContent = currentHomeScore;
+}
+
+function add3Point() {
+  currentHomeScore = currentHomeScore + 3;
+  displayHomeScore.textContent = currentHomeScore;
+}
+
+// manipulate guest scores
+const displayGuestScore = document.getElementById("guest-score");
+displayGuestScore.textContent = currentGuestScore;
+
+const add1Btn = document.getElementById("1-point");
+const add2Btn = document.getElementById("2-point");
+const add3Btn = document.getElementById("3-point");
+
+add1Btn.addEventListener("click", () => {
+  currentGuestScore = currentGuestScore + 1;
+  displayGuestScore.textContent = currentGuestScore;
+});
+
+add2Btn.addEventListener("click", () => {
+  currentGuestScore = currentGuestScore + 2;
+  displayGuestScore.textContent = currentGuestScore;
+});
+
+add3Btn.addEventListener("click", () => {
+  currentGuestScore = currentGuestScore + 3;
+  displayGuestScore.textContent = currentGuestScore;
+});
