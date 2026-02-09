d60255d669d934173ee868947916196d71b80589
Michael Mensah <mkboatmens17@gmail.com>
2026-02-09 18:51:33 +0000

    feat: homepage display component

---
+++ react-js/movie-app/src/pages/Home.jsx
@@ -0,0 +1,22 @@
+import MovieCard from "../components/MovieCard"
+
+function Home() {
+
+	const movies = [
+		{id:1, title: "John Wick", release_dat="2020"},
+		{id:2, title: "Terminator", release_date="199"},
+		{id:3, title: "The Matrix", release_date="1998"}
+	]
+
+	return (
+	<div className="home">
+		<div className="movies-grid">
+		{movies.map((movie) => (
+			<MovieCard movie= {movie} key={movie.id}/> 
+		))}	
+		</div>
+		</div>
+	);
+}
+
+export default Home;
