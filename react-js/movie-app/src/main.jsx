9b43c1b7dc1ee251ddfb63aaddb1643dc082b59f
Michael Boateng Mensah <mkboatmens17@gmail.com>
2026-02-10 05:43:48 +0000

    fix: rendering of homepage

--- react-js/movie-app/src/main.jsx
+++ react-js/movie-app/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import MovieCard from "./components/MovieCard.jsx";
+import Home from "./pages/Home.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- react-js/movie-app/src/pages/Home.jsx
+++ react-js/movie-app/src/pages/Home.jsx
@@ -1,22 +1,21 @@
-import MovieCard from "../components/MovieCard"
+import MovieCard from "../components/MovieCard";
 
 function Home() {
+  const movies = [
+    { id: 1, title: "John Wick", release_date: "2020" },
+    { id: 2, title: "Terminator", release_date: "199" },
+    { id: 3, title: "The Matrix", release_date: "1998" },
+  ];
 
-	const movies = [
-		{id:1, title: "John Wick", release_dat="2020"},
-		{id:2, title: "Terminator", release_date="199"},
-		{id:3, title: "The Matrix", release_date="1998"}
-	]
-
-	return (
-	<div className="home">
-		<div className="movies-grid">
-		{movies.map((movie) => (
-			<MovieCard movie= {movie} key={movie.id}/> 
-		))}	
-		</div>
-		</div>
-	);
+  return (
+    <div className="home">
+      <div className="movies-grid">
+        {movies.map((movie) => (
+          <MovieCard movie={movie} key={movie.id} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
