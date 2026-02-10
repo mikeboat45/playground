ac1b1d3f468c584c454af98f54691b3058cb1d89
Michael Boateng Mensah <mkboatmens17@gmail.com>
2026-02-10 20:37:02 +0000

    add navbar, add css styles

--- react-js/movie-app/src/App.jsx
+++ react-js/movie-app/src/App.jsx
@@ -1,16 +1,20 @@
-import "./App.css";
+import "./css/App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Favorites from "./pages/Favorites.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 function App() {
   return (
-    <main className="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-      </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main className="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
