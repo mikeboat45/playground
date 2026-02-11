ce1c4eb2b56c679d53a47a855cc083eb522d0c72
Michael Boateng Mensah <mkboatmens17@gmail.com>
2026-02-11 16:39:53 +0000

    handleSearch logic to allow searching through movies

--- react-js/movie-app/src/pages/Home.jsx
+++ react-js/movie-app/src/pages/Home.jsx
@@ -25,9 +25,22 @@ function Home() {
     loadPopularMovies();
   }, []);
 
-  const handleSearch = (e) => {
+  const handleSearch = async (e) => {
     e.preventDefault();
-    alert(searchQuery);
+    if (!searchQuery.trim()) return;
+    if (loading) return;
+
+    setLoading(true);
+    try {
+      const searchResults = await searchMovies(searchQuery);
+      setMovies(searchResults);
+      setError(null);
+    } catch {
+      console.log(err);
+      setError("Failed to search movies...");
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
